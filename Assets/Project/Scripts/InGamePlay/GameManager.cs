1ab6e71ac0e2dee8d83e468303eb049530edd82a
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-08-27 23:39:03 +0900

    [Add]GameManager

---
+++ Assets/Project/Scripts/InGamePlay/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Hikanyan.Core;
+
+public class GameManager:AbstractSingleton<GameManager>
+{
+        private VampireSurvivor _vampireSurvivor;
+
+        protected override void OnAwake()
+        {
+                _vampireSurvivor = new VampireSurvivor();
+        }
+}
---
+++ Assets/Project/Scripts/InGamePlay/VampireSurvivor.cs
@@ -0,0 +1,35 @@
+﻿using Hikanyan.Core;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class VampireSurvivor : AbstractSingleton<VampireSurvivor>
+{
+    private ObjectPool _enemyPool;
+    public Transform player;
+    public float spawnDistance = 3.0f; // プレイヤーからの距離
+
+    void Start()
+    {
+        _enemyPool = GetComponent<ObjectPool>();
+
+        // プレイヤーの位置を監視
+        player.OnCollisionEnter2DAsObservable()
+            .Subscribe(_ =>
+            {
+                // プレイヤーに接触したら敵を生成
+                GameObject enemy = _enemyPool.GetObject();
+                if (enemy != null)
+                {
+                    // プレイヤーの位置を取得
+                    Vector3 playerPosition = player.position;
+
+                    // プレイヤーの前方に敵を生成する例（プレイヤーの前方向ベクトル * 距離を加算）
+                    Vector3 spawnPosition = playerPosition + player.forward * spawnDistance;
+
+                    enemy.transform.position = spawnPosition;
+                }
+            })
+            .AddTo(this); // オブジェクトが破棄されたらサブスクリプションも破棄
+    }
+}
